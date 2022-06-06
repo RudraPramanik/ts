68a2966bb2d377428cf2df4f0b48ca8e79dcd167
rudra pramanik <rudrahimangshoo2020@gmail.com>
2022-06-06 12:44:49 +0600

    0.1.2

--- structure.js
+++ structure.js
@@ -4,3 +4,12 @@ function logPoint(p) {
 // logs "12, 26"
 var point = { x: 12, y: 26 };
 logPoint(point);
+var VirtualPoint = /** @class */ (function () {
+    function VirtualPoint(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return VirtualPoint;
+}());
+var newVPoint = new VirtualPoint(13, 56);
+logPoint(newVPoint); // logs "13, 56"
--- structure.ts
+++ structure.ts
@@ -10,3 +10,16 @@ function logPoint(p: Point) {
 // logs "12, 26"
 const point = { x: 12, y: 26 };
 logPoint(point);
+
+class VirtualPoint {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const newVPoint = new VirtualPoint(13, 56);
+logPoint(newVPoint); // logs "13, 56"
