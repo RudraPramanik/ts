a857457c4f524d8a2db192509a7fb9dc40a7f37f
rudra pramanik <rudrahimangshoo2020@gmail.com>
2022-06-06 12:41:16 +0600

    0.1.1

---
+++ structure.js
@@ -0,0 +1,6 @@
+function logPoint(p) {
+    console.log("".concat(p.x, ", ").concat(p.y));
+}
+// logs "12, 26"
+var point = { x: 12, y: 26 };
+logPoint(point);
---
+++ structure.ts
@@ -0,0 +1,12 @@
+interface Point {
+  x: number;
+  y: number;
+}
+
+function logPoint(p: Point) {
+  console.log(`${p.x}, ${p.y}`);
+}
+
+// logs "12, 26"
+const point = { x: 12, y: 26 };
+logPoint(point);
